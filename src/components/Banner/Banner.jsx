2a136620e458d48a756c4f4f829ef966ebf25895
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-02 23:33:20 +0600

    Banner section added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import bgShadow from '../../assets/images/bg-shadow.png'
+import banner from '../../assets/images/banner-main.png'
+
+
+const Banner = () => {
+    return (
+        <div className='mt-10'>
+            <img className='bg-black rounded-3xl absolute' src={bgShadow} alt="" />
+            <div className='relative text-white text-center px-auto space-y-2 md:space-y-5 lg:space-y-8 lg:py-20 p-2'>
+                <img className='mx-auto w-20 lg:w-60' src={banner} alt="" />
+                <h1 className='lg:text-4xl font-bold'>Assemble Your Ultimate BPL Cricket Team</h1>
+                <p className='text-sm lg:text-xl text-gray-400'>Beyond Boundaries Beyond Limits</p>
+                <button class="btn btn-soft bg-[#E7FE29] px-2 text-sm">Claim Free Credit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
